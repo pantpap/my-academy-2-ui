d4bb9c3834059bc4cddbc951f5d3041751e9c125
Pantelis Papasavvas <pantpap@gmail.com>
2026-04-21 18:35:52 +0300

    feat(): change application name

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 
@@ -7,7 +7,8 @@ import { Header } from './shared/components/header/header';
   imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('material-recipe');
+  protected readonly title = signal('Academy');
 }
