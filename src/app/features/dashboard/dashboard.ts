fdffdc86938308d4204969a684d0ec3ea44064da
Pantelis Papasavvas <pantpap@gmail.com>
2026-06-01 12:42:30 +0300

    feat(): optimize dashboard component with OnPush change detection strategy

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {}
