234de0e601f68eae999fbad0aa7e5a11dd1868bc
Pantelis Papasavvas <pantpap@gmail.com>
2026-06-02 16:26:13 +0300

    feat(): apply auth guard on lyout routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -9,6 +10,7 @@ export const routes: Routes = [
   },
   {
     path: 'app',
+    canActivate: [authGuard],
     loadChildren: () => import('./core/layout/layout-routes').then((m) => m.layoutRoutes),
   }
 ];
