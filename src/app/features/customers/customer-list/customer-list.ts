90c17ff6bcf3262f5421d3c93903b64ba811083a
Pantelis Papasavvas <pantpap@gmail.com>
2026-06-02 18:13:07 +0300

    feat(): implement customer list table with data binding and actions

--- src/app/features/customers/customer-list/customer-list.ts
+++ src/app/features/customers/customer-list/customer-list.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, ResourceRef } from '@angular/core';
+import { MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow,
+  MatHeaderRowDef, MatRow, MatRowDef, MatTable } from '@angular/material/table';
+import { Customer, CustomersPagedResponse } from '../../../common/interfaces/customer';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatIcon,
+  ],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomerList {}
+export class CustomerList {
+  readonly dataSourceResourceValue =
+    input.required<ResourceRef<CustomersPagedResponse | undefined>>();
+
+  displayedColumns: string[] = ['name', 'birthDate', 'gender', 'phone', 'sport', 'actions'];
+
+  onRowClick(customer: Customer) {
+    console.log(customer);
+  //   TODO: navigate to customer details using id
+
+  }
+}
